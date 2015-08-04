24317b7a109b156d2ed934477b7e5831dfdfcd16
anx <anx@laptop.anx>
2015-08-04 16:48:04 +0800

    fix some bug; ver 0.2

--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 rootUrl = 'http://pyqt.sourceforge.net/Docs/PyQt4/'
 
@@ -11,17 +12,20 @@ classList = []
 MD = open('README.md', 'w+')
 
 r = requests.get(rootUrl+'classes.html', proxies=proxy)
+# r = requests.get(rootUrl+'classes.html')
 soup = BeautifulSoup(r.text)
 for a in soup.findAll('table')[1].findAll('a'):
     classList.append(a['href'])
 
-for url in classList:
-    res = requests.get(rootUrl+url, proxies=proxy)
-    innerSoup = BeautifulSoup(res.text)
-    if innerSoup.findAll('img', align=None):
-        MD.write('#' + url + '\n')
-        for img in innerSoup.findAll('img', align=None):
-            MD.write('![' + url +'](' + rootUrl + img['src'] + ')\n')
-
-MD.close()
+try:
+    for url in classList:
+        res = requests.get(rootUrl+url, proxies=proxy)
+        innerSoup = BeautifulSoup(res.text)
+        if innerSoup.findAll('img', align=None):
+            MD.write('[' + url + '](' + rootUrl + url + ')\n')
+            for img in innerSoup.findAll('img', align=None):
+                MD.write('![' + url +'](' + rootUrl + img['src'] + ')\n')
+        time.sleep(0.1)
+finally:
+    MD.close()
 
